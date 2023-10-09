175deeab6a511c54507cae84482d84973d44ad79
sindyhernandez2024 <sindy.hernandez2024@hilasal.superate.org.sv>
2023-10-09 08:17:39 -0600

    actualizacion index

--- src/main.js
+++ src/main.js
@@ -1,26 +1,99 @@
 const taskForm = document.getElementById("task-form");
 const taskList = document.getElementById("task-list");
 
+const titleInput = document.getElementById("task-title");
+const descriptionInput = document.getElementById("task-description");
+
+const MAX_TASKS = 10; // Establecer un límite máximo de tareas
+
+class Task {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+    }
+
+    toggleCompletion() {
+        this.completed = !this.completed;
+    }
+}
+
+// Función para cargar tareas desde el almacenamiento local
+function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    for (const task of tasks) {
+        addTaskToDOM(task);
+    }
+}
+
+// Función para guardar tareas en el almacenamiento local
+function saveTasks(tasks) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// Función para agregar una tarea al DOM
+function addTaskToDOM(task) {
+    const taskItem = document.createElement("li");
+    const completeButton = document.createElement("button");
+    completeButton.className = "complete-button";
+    completeButton.innerText = task.completed ? "✔️" : "Completar";
+
+    completeButton.addEventListener("click", function () {
+        task.toggleCompletion();
+        completeButton.innerText = task.completed ? "✔️" : "Completar";
+        saveTasks(tasks);
+    });
+
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "delete-button";
+    deleteButton.innerText = "Eliminar";
+
+    deleteButton.addEventListener("click", function () {
+        taskItem.remove();
+        const taskIndex = tasks.indexOf(task);
+        if (taskIndex !== -1) {
+            tasks.splice(taskIndex, 1);
+            saveTasks(tasks);
+        }
+    });
+
+    taskItem.innerHTML = `
+        <h3>${task.title}</h3>
+        <p>${task.description}</p>
+    `;
+
+    taskItem.appendChild(completeButton);
+    taskItem.appendChild(deleteButton);
+
+    taskList.appendChild(taskItem);
+}
+
+// Cargar tareas al cargar la página
+const tasks = [];
+loadTasks();
+
 taskForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const taskTitle = document.getElementById("task-title").value;
-    const taskDescription = document.getElementById("task-description").value;
+    const taskTitle = titleInput.value.trim();
+    const taskDescription = descriptionInput.value.trim();
 
-    if (taskTitle.trim() === "") {
+    if (taskTitle === "") {
         alert("El título de la tarea no puede estar vacío.");
         return;
     }
 
-    const taskItem = document.createElement("li");
-    taskItem.innerHTML = `
-        <h3>${taskTitle}</h3>
-        <p>${taskDescription}</p>
-        <button class="complete-button">Completar</button>
-        <button class="delete-button">Eliminar</button>
-    `;
+    // Verificar si se ha alcanzado el límite máximo de tareas
+    if (taskList.childElementCount >= MAX_TASKS) {
+        alert("Se ha alcanzado el límite máximo de tareas.");
+        return;
+    }
 
-    taskList.appendChild(taskItem);
+    const task = new Task(taskTitle, taskDescription);
+    tasks.push(task);
+    saveTasks(tasks);
+
+    addTaskToDOM(task);
 
     // Limpiar el formulario
     taskForm.reset();
